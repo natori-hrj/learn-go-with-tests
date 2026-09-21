f2680aa5f8a5943ff63354e71fe99102fe072d9a
natori-hrj <natori.rj@gmail.com>
2026-09-21 11:46:13 +0900

    01

--- 01-hello/hello_test.go
+++ 01-hello/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	asserCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", want, got)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		asserCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		asserCorrectMessage(t, got, want)
+	})
 }
